19f29f01299ba08b8221f3dfa0fce3fd6e6e3a2c
Yosuke Adachi <adachi@eku.soy>
2018-07-10 23:28:26 +0100

    The Flask Mega-Tutorial Part VII: Error Handling - 2

--- app/forms.py
+++ app/forms.py
@@ -34,3 +34,15 @@ class EditProfileForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
     submit = SubmitField('Submit')
+
+    def __init__(self, original_username, *arg, **kwargs):
+        supper(EditProfileForm, self).__init__(*arg, **kwargs)
+        self.original_username = original_username
+
+    def validiate_username(self, username):
+        if username.data != self.original_username:
+            user = User.query.filter_by(username=self.username.data).first()
+            if user is not None:
+                raise ValidiationError('Please use different username.')
+          
+
