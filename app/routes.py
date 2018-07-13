a44fd79a4741cc10a198eccac3324f65319a4e99
Yosuke Adachi <adachi@eku.soy>
2018-07-13 15:27:38 +0100

    The Flask Mega-Tutorial Part IX: Pagination - 2

--- app/routes.py
+++ app/routes.py
@@ -7,7 +7,6 @@ from werkzeug.urls import url_parse
 from datetime import datetime
 
 
-
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 @login_required
@@ -19,8 +18,16 @@ def index():
         db.session.commit()
         flash('Your post is now live!')
         return redirect(url_for('index'))
-    posts = current_user.followed_posts().all()
-    return render_template('index.html', title='Home Page', form=form, posts=posts)
+    page = request.args.get('page', 1, type=int)
+    posts = current_user.followed_posts().paginate(page, app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('index', page=posts.next_num) if posts.has_next else None
+    prev_url = url_for('index', page=posts.prev_num) if posts.has_prev else None
+    return render_template( 'index.html',
+                            title='Home Page',
+                            form=form,
+                            posts=posts.items,
+                            next_url=next_url,
+                            prev_url=prev_url)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -71,11 +78,13 @@ def user(username):
     ]
     return render_template('user.html', user=user, posts=posts)
 
+
 @app.before_request
 def before_request():
     if current_user.is_authenticated:
         current_user.last_seen = datetime.utcnow()
         db.session.commit()
+
 
 @app.route('/edit_profile', methods=['GET', 'POST'])
 @login_required
@@ -85,13 +94,14 @@ def edit_profile():
         current_user.username = form.username.data
         current_user.about_me = form.about_me.data
         db.session.commit()
-        flash('Your changes have been saved.') 
+        flash('Your changes have been saved.')
         return redirect(url_for('edit_profile'))
     elif request.method == 'GET':
         form.username.data = current_user.username
         form.about_me.data = current_user.about_me
     return render_template('edit_profile.html', title='Edit Profile', form=form )
-        
+
+
 @app.route('/follow/<username>')
 @login_required
 def follow(username):
@@ -107,6 +117,7 @@ def follow(username):
     flash('You are following {}!'.format(username))
     return redirect(url_for('user', username=username))
 
+
 @app.route('/unfollow/<username>')
 @login_required
 def unfollow(username):
@@ -121,3 +132,19 @@ def unfollow(username):
     db.session.commit()
     flash('You are not following {}!'.format(username))
     return redirect(url_for('user', username=username))
+
+
+@app.route('/explore')
+@login_required
+def explore():
+    page = request.args.get('page', 1, type=int)
+    paginate(page, app.config['POSTS_PER_PAGE'], False)
+    posts = Post.query.order_by(Post.timestamp.desc()).paginate(page, app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('explore', page=posts.next_num) if posts.has_next else None
+    prev_url = url_for('explore', page=posts.prev_num) if posts.has_prev else None
+    return render_template( 'index.html',
+                            title='Explore',
+                            form=form,
+                            posts=posts.items,
+                            next_url=next_url,
+                            prev_url=prev_url)
