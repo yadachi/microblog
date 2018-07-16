d02bfce229bc7e5d792c174036d0a39d74809adb
Yosuke Adachi <adachi@eku.soy>
2018-07-16 07:00:16 +0100

    The Flask Mega-Tutorial Part IX: Pagination - 3

--- app/routes.py
+++ app/routes.py
@@ -72,11 +72,11 @@ def register():
 @app.route('/user/<username>')
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
-    posts = [
-        {'author': user, 'body': 'Test post #1'},
-        {'author': user, 'body': 'Test post #2'}
-    ]
-    return render_template('user.html', user=user, posts=posts)
+    page = request.args.get('page', 1, type=int)
+    posts = user.posts.order_by(Post.timestamp.desc()).paginate(page, app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('user', username=user.username, page=posts.next_num) if posts.has_next else None
+    prev_url = url_for('user', username=user.username, page=posts.prev_num) if posts.has_prev else None
+    return render_template('user.html', user=user, posts=posts.items, next_url=next_url, prev_url=prev_url)
 
 
 @app.before_request
@@ -138,13 +138,11 @@ def unfollow(username):
 @login_required
 def explore():
     page = request.args.get('page', 1, type=int)
-    paginate(page, app.config['POSTS_PER_PAGE'], False)
     posts = Post.query.order_by(Post.timestamp.desc()).paginate(page, app.config['POSTS_PER_PAGE'], False)
     next_url = url_for('explore', page=posts.next_num) if posts.has_next else None
     prev_url = url_for('explore', page=posts.prev_num) if posts.has_prev else None
     return render_template( 'index.html',
                             title='Explore',
-                            form=form,
                             posts=posts.items,
                             next_url=next_url,
                             prev_url=prev_url)
--- config.py
+++ config.py
@@ -12,4 +12,4 @@ class Config(object):
     MAIL_USERNAME  = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD  = os.environ.get('MAIL_PASSWORD')
     ADMINS         = ['adachi@eku.soy']
-    POSTS_PER_PAGE = 3
+    POSTS_PER_PAGE = 25
