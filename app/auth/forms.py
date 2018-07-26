0c741a7f18e79a7e9aeadd1fc5d44293ca0e61c5
Yosuke Adachi <adachi@eku.soy>
2018-07-26 16:15:27 +0100

    The Flask Mega-Tutorial Part XV: A Better Application Structure - 2

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from flask_babel import _, lazy_gettext as _l
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
-from wtforms.validators import DataRequired, ValidationError, Email, EqualTo, Length
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
 from app.models import User
 
 class LoginForm(FlaskForm):
@@ -29,26 +29,6 @@ class RegistrationForm(FlaskForm):
         user = User.query.filter_by(email=email.data).first()
         if user is not None:
             raise ValidationError(_('Please use a different email address'))
-
-
-class EditProfileForm(FlaskForm):
-    username = StringField(_l('Username'), validators=[DataRequired()])
-    about_me = TextAreaField(_l('About me'), validators=[Length(min=0, max=140)])
-    submit = SubmitField(_l('Submit'))
-
-    def __init__(self, original_username, *arg, **kwargs):
-        super(EditProfileForm, self).__init__(*arg, **kwargs)
-        self.original_username = original_username
-
-    def validiate_username(self, username):
-        if username.data != self.original_username:
-            user = User.query.filter_by(username=self.username.data).first()
-            if user is not None:
-                raise ValidiationError(_('Please use different username.'))
-
-class PostForm(FlaskForm):
-    post = TextAreaField(_l('Say Something'), validators=[ DataRequired(), Length(min=1, max=140) ])
-    submit = SubmitField(_l('submit'))
 
 
 class ResetPasswordRequestForm(FlaskForm):
